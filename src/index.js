7a4dc067e50ef1634f7425ff8e784db3ebf2af04
Maksym <maxthebest1906@gmail.com>
2024-02-12 20:52:09 +0200

    init project

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { theme } from 'constants/theme';
+import { ThemeProvider } from '@emotion/react';
+import './index.css';
+import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      {/* <Provider store={store}> */}
+        {/* <PersistGate loading={null} persistor={persistor}> */}
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        {/* </PersistGate> */}
+      {/* </Provider> */}
+    </ThemeProvider>
+  </React.StrictMode>
+);
